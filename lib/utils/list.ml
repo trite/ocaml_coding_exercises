f531f4a319e60113f807737b91d517f317636e60
Paul Brunner <trite@users.noreply.github.com>
2023-04-01 12:12:26 -0700

    Create list splitting function that will be needed later

---
+++ lib/utils/list.ml
@@ -0,0 +1,11 @@
+let split_on_value split_on to_split =
+  let rec go acc curr split_on to_split =
+    match to_split with
+    | [] ->
+        Stdlib.List.rev (curr :: acc)
+    | hd :: tl when hd = split_on ->
+        go (Stdlib.List.rev curr :: acc) [] split_on tl
+    | hd :: tl ->
+        go acc (hd :: curr) split_on tl
+  in
+  go [] [] split_on to_split
