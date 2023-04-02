790e5db0f5158f59c0f034fd5633caea925e4973
Paul Brunner <trite@users.noreply.github.com>
2023-04-02 09:59:29 -0700

    Still figuring out what I want this to look like
    Seriously, who needs test frameworks? This is fine https://giphy.com/embed/QMHoU66sBXqqLqYvGO

--- src/utils/v.ml
+++ src/utils/v.ml
@@ -1,4 +1,4 @@
-(* let verify x = assert x *)
+(* Custom functions for verification/assertion *)
 
 let verify yday failure_message x =
   if x then () else failwith (failure_message yday)
@@ -11,3 +11,11 @@ let p1data yday = yday ^ " p1 actual data failed"
 let p2test yday = yday ^ " p2 test data failed"
 
 let p2data yday = yday ^ " p2 actual data failed"
+
+let verify_p1_test yday = verify yday p1test
+
+let verify_p1_data yday = verify yday p1data
+
+let verify_p2_test yday = verify yday p2test
+
+let verify_p2_data yday = verify yday p2data
